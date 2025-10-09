a6e5857dd1c5a8a5024b841fd55f7b28b3214160
giannencljsn <giannejuson16@gmail.com>
2025-10-09 14:23:05 +0800

    +migrated database table "Category"

--- GijuBedAndBreakfast/Data/ApplicationDbContext.cs
+++ GijuBedAndBreakfast/Data/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GijuBedAndBreakfast.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GijuBedAndBreakfast.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        // ctor for DI
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-
         }
 
+        // DbSets must be declared as class members (not inside the ctor)
+        public DbSet<Category> Categories { get; set; }
     }
 }
